775b4fa1738af0e8478b85ddec6351371aa8c5e7
Julie Winchester <julie.winchester@duke.edu>
2018-02-15 12:26:10 -0500

    Created ms media file class, changed some data associations, only export published media

--- ms_uf_ac_export.py
+++ ms_uf_ac_export.py
@@ -4,6 +4,7 @@
 # February 14, 2018
 
 import credentials
+import ms_media_file
 import pandas
 import phpserialize
 import pymysql
@@ -20,68 +21,6 @@ def db_conn():
 def db_query(cursor, sql, args):
 	cursor.execute(sql, [args])
 	return cursor.fetchall()
-
-def blob_to_array(blob):
-	return phpserialize.unserialize(zlib.decompress(blob))
-
-def creator_string(mf):
-	return mf['fname'] + " " + mf['lname'] + " <" + mf['email'] + ">" 
-
-def copyright_permission(mf):
-	return {
-		0: 'Copyright permission not set',
-		1: 'Person loading media owns copyright and grants permission for use of media on MorphoSource',
-		2: 'Permission to use media on MorphoSource granted by copyright holder',
-		3: 'Permission pending',
-		4: 'Copyright expired or work otherwise in public domain',
-		5: 'Copyright permission not yet requested'
-	}[int(mf['copyright_permission'])]
-
-def copyright_license(mf):
-	return {
-		0: 'Media reuse policy not set',
-		1: 'CC0 - relinquish copyright',
-		2: 'Attribution CC BY - reuse with attribution',
-		3: 'Attribution-NonCommercial CC BY-NC - reuse but noncommercial',
-		4: 'Attribution-ShareAlike CC BY-SA - reuse here and applied to future uses',
-		5: 'Attribution- CC BY-NC-SA - reuse here and applied to future uses but noncommercial',
-		6: 'Attribution-NoDerivs CC BY-ND - reuse but no changes',
-		7: 'Attribution-NonCommercial-NoDerivs CC BY-NC-ND - reuse noncommerical no changes',
-		8: 'Media released for onetime use, no reuse without permission',
-		20: 'Unknown - Will set before project publication'
-	}[int(mf['copyright_license'])]
-
-def copyright_license_uri(mf):
-	return {
-		0: '',
-		1: 'https://creativecommons.org/publicdomain/zero/1.0/',
-		2: 'https://creativecommons.org/licenses/by/3.0/',
-		3: 'https://creativecommons.org/licenses/by-nc/3.0/',
-		4: 'https://creativecommons.org/licenses/by-sa/3.0/',
-		5: 'https://creativecommons.org/licenses/by-nc-sa/3.0/',
-		6: 'https://creativecommons.org/licenses/by-nd/3.0/',
-		7: 'https://creativecommons.org/licenses/by-nc-nd/3.0/',
-		8: '',
-		20: ''
-	}[int(mf['copyright_license'])]
-
-def copyright_license_logo_uri(mf):
-	return {
-		0: '',
-		1: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-nd.eu.png',
-		2: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png',
-		3: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc.png',
-		4: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png',
-		5: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-sa.png',
-		6: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nd.png',
-		7: 'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-nd.png',
-		8: '',
-		20: ''
-	}[int(mf['copyright_license'])]
-
-def citation_instructions(mf):
-	if mf['media_citation_instruction1']:
-		return mf['media_citation_instruction1'] + " provided access to these data " + mf['media_citation_instruction2'] + " " + mf['media_citation_instruction3'] + ". The files were downloaded from www.morphosource.org, Duke University."
 
 specimen_uuids = pandas.read_csv('uf_herp_specimen_uuids.csv')
 uuid_list = list(specimen_uuids['uuid'])
@@ -130,85 +69,14 @@ ac = pandas.DataFrame(columns=
 	'xmpRights:WebStatement',
 	'ac:licenseLogoURL',
 	'photoshop:Credit',
-	'coreid']);
+	'coreid'])
 
-for mf in r:
-	mf_info = blob_to_array(mf['mf.media'])
+for mf_dict in r:
+	mf = ms_media_file.MsMediaFile(mf_dict)
 
-	# Create media file dict
-	d = {
-		'dcterms:identifier': mf['ark'], 
-		'ac:associatedSpecimenReference': mf['uuid'],
-		'ac:providerManagedID': mf['media_file_id'],
-		'ac:derivedFrom': mf['derived_from_media_file_id'],
-		'ac:providerLiteral': 'MorphoSource',
-		'ac:provider': 'http://www.morphosource.org',
-		'dc:type': 'Image',
-		'dcterms:type': 'http://purl.org/dc/dcmitype/Image',
-		'ac:subtypeLiteral': '', # mf['modality'] when implemented
-		'ac:subtype': '', # need to have function to grab this when implemented
-		'ac:accessURI': 'http://www.morphosource.org/index.php/Detail/MediaDetail/Show/media_file_id/' + str(mf['media_file_id']),
-		'dc:format': mf_info['original']['MIMETYPE'],
-		'ac:subjectPart': mf['mf.element'],
-		'ac:subjectOrientation': mf['mf.side'],
-		'ac:caption': mf['mf.notes'],
-		'Iptc4xmpExt:LocationCreated': mf['name'],
-		'ac:captureDevice': mf['sc.name'],
-		'dc:creator': creator_string(mf),
-		'ms:scanningTechnician': mf['scanner_technicians'],
-		'ac:fundingAttribution': mf['grant_support'],
-		'exif:Xresolution': mf['scanner_x_resolution'],
-		'exif:Yresolution': mf['scanner_y_resolution'],
-		'dicom:SpacingBetweenSlices': mf['scanner_z_resolution'],
-		'dc:rights': copyright_permission(mf),
-		'dcterms:rights': copyright_license_uri(mf),
-		'xmpRights:Owner': mf['copyright_info'],
-		'xmpRights:UsageTerms': copyright_license(mf),
-		'xmpRights:WebStatement': copyright_license_uri(mf),
-		'ac:licenseLogoURL': copyright_license_logo_uri(mf),
-		'photoshop:Credit': citation_instructions(mf),
-		'coreid': mf['occurrence_id']
-	}
-
-	p_url = "http://www.morphosource.org/media/morphosource/images/" + mf_info['large']['HASH'] + "/" + mf_info['large']['FILENAME']
-
-	# Create media file preview image dict
-	p = {
-		'dcterms:identifier': p_url, 
-		'ac:associatedSpecimenReference': mf['uuid'],
-		'ac:providerManagedID': str(mf['media_file_id']) + 'p',
-		'ac:derivedFrom': d['dcterms:identifier'],
-		'ac:providerLiteral': 'MorphoSource',
-		'ac:provider': 'http://www.morphosource.org',
-		'dc:type': 'StillImage',
-		'dcterms:type': 'http://purl.org/dc/dcmitype/StillImage',
-		'ac:subtypeLiteral': 'Graphic',
-		'ac:subtype': 'https://terms.tdwg.org/wiki/AC_Subtype_Examples',
-		'ac:accessURI': p_url,
-		'dc:format': mf_info['large']['MIMETYPE'],
-		'ac:subjectPart': mf['mf.element'],
-		'ac:subjectOrientation': mf['mf.side'],
-		'ac:caption': mf['mf.notes'],
-		'Iptc4xmpExt:LocationCreated': mf['name'],
-		'ac:captureDevice': mf['sc.name'],
-		'dc:creator': creator_string(mf),
-		'ms:scanningTechnician': mf['scanner_technicians'],
-		'ac:fundingAttribution': mf['grant_support'],
-		'exif:Xresolution': mf_info['large']['WIDTH'],
-		'exif:Yresolution': mf_info['large']['HEIGHT'],
-		'dicom:SpacingBetweenSlices': '',
-		'dc:rights': copyright_permission(mf),
-		'dcterms:rights': copyright_license_uri(mf),
-		'xmpRights:Owner': mf['copyright_info'],
-		'xmpRights:UsageTerms': copyright_license(mf),
-		'xmpRights:WebStatement': copyright_license_uri(mf),
-		'ac:licenseLogoURL': copyright_license_logo_uri(mf),
-		'photoshop:Credit': citation_instructions(mf),
-		'coreid': mf['occurrence_id']
-	}
-
-	ac = ac.append(d, ignore_index=True)
-	ac = ac.append(p, ignore_index=True)
+	if mf.is_published():	
+		ac = ac.append(mf.ac_mf_dict, ignore_index=True)
+		ac = ac.append(mf.ac_mfp_dict, ignore_index=True)
 
 ac.to_csv('output.csv', index=False, index_label=False)
 
